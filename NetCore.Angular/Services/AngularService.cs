7e8c08a6b5181e1a9654a7947332557b078d8d21
Neville <live.neville@live.com>
2018-03-27 04:59:46 -0400

    initial taghelpers and service

---
+++ NetCore.Angular/Services/AngularService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetCore.Angular.Services
+{
+    public class AngularService
+    {
+        private readonly IHtmlHelper html;
+
+        Dictionary<string, object> Pairs { get; set; }
+
+        AngularService(IHtmlHelper html)
+        {
+            Pairs = new Dictionary<string, object>();
+            this.html = html;
+        }
+
+        public IHtmlContent GenerateScripts()
+        {
+
+            string Scripts = string.Join(@",\n",
+                        Pairs.Select(p => $"'{p.Key}' : {html.Raw(JsonConvert.SerializeObject(p.Value))}")
+                );
+
+            return html.Raw($"<script>var angular_pairs = {{{Scripts}}};</script>");
+        }
+
+    }
+}
--- NetCore.Angular/TagHelpers/AngularTagHelper.cs
+++ NetCore.Angular/TagHelpers/AngularTagHelper.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using NetCore.Angular.Services;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,11 +9,19 @@ namespace NetCore.Angular.TagHelpers
 
     public abstract class AngularTagHelper : TagHelper
     {
+        private readonly AngularService angularService;
 
         internal abstract string Tag { get; }
 
         public object Source { get; set; }
-        
+
+        public string ScopeDest { get; set; }
+
+        public AngularTagHelper(AngularService angularService)
+        {
+            this.angularService = angularService;
+        }
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             output.TagName = Tag;
@@ -22,43 +31,129 @@ namespace NetCore.Angular.TagHelpers
 
     public class DivAngularTagHelper : AngularTagHelper
     {
+        public DivAngularTagHelper(AngularService angularService) : base(angularService)
+        {
+        }
+
         internal override string Tag => "div";
+    }
+
+    public class TableAngularTagHelper : AngularTagHelper
+    {
+        public TableAngularTagHelper(AngularService angularService) : base(angularService)
+        {
+        }
+
+        internal override string Tag => "Table";
+    }
+
+    public class TrAngularTagHelper : AngularTagHelper
+    {
+        public TrAngularTagHelper(AngularService angularService) : base(angularService)
+        {
+        }
+
+        internal override string Tag => "Tr";
+    }
+
+    public class LiAngularTagHelper : AngularTagHelper
+    {
+        public LiAngularTagHelper(AngularService angularService) : base(angularService)
+        {
+        }
+
+        internal override string Tag => "Li";
+    }
+
+    public class UlAngularTagHelper : AngularTagHelper
+    {
+        public UlAngularTagHelper(AngularService angularService) : base(angularService)
+        {
+        }
+
+        internal override string Tag => "Ul";
+    }
+
+    public class OlAngularTagHelper : AngularTagHelper
+    {
+        public OlAngularTagHelper(AngularService angularService) : base(angularService)
+        {
+        }
+
+        internal override string Tag => "Ol";
+    }
+
+    public class FooterAngularTagHelper : AngularTagHelper
+    {
+        public FooterAngularTagHelper(AngularService angularService) : base(angularService)
+        {
+        }
+
+        internal override string Tag => "Footer";
     }
 
     public class SpanAngularTagHelper : AngularTagHelper
     {
+        public SpanAngularTagHelper(AngularService angularService) : base(angularService)
+        {
+        }
+
         internal override string Tag => "span";
     }
 
     public class PAngularTagHelper : AngularTagHelper
     {
+        public PAngularTagHelper(AngularService angularService) : base(angularService)
+        {
+        }
+
         internal override string Tag => "p";
     }
 
     public class H1AngularTagHelper : AngularTagHelper
     {
+        public H1AngularTagHelper(AngularService angularService) : base(angularService)
+        {
+        }
+
         internal override string Tag => "h1";
     }
 
     public class H2AngularTagHelper : AngularTagHelper
     {
+        public H2AngularTagHelper(AngularService angularService) : base(angularService)
+        {
+        }
+
         internal override string Tag => "h2";
     }
 
     public class H3AngularTagHelper : AngularTagHelper
     {
+        public H3AngularTagHelper(AngularService angularService) : base(angularService)
+        {
+        }
+
         internal override string Tag => "h3";
     }
 
     public class H4AngularTagHelper : AngularTagHelper
     {
+        public H4AngularTagHelper(AngularService angularService) : base(angularService)
+        {
+        }
+
         internal override string Tag => "h4";
     }
 
     public class H5AngularTagHelper : AngularTagHelper
     {
+        public H5AngularTagHelper(AngularService angularService) : base(angularService)
+        {
+        }
+
         internal override string Tag => "h5";
     }
-
+    
 
 }
